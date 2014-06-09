2c8da4ca7516deb91534bd61af7c7cb951f648af
Nagachandra <nagachandra.kn89@gmail.com>
2014-06-09 20:43:21 +0530

    Built the data structurs using Active Record

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :location, :excerpt, :location, :body, :published_at, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'bc2b76666fcee30a6b67ed61952829230cf0e31a7f095ddccf5e300d614f7ab565d8b5aabc6947dae7103886502d0eb8dd112cc778657fb2e1a502813ca5bdfc'
